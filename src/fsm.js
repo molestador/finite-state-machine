5c51266591af435faca7af2ca4a0cfddad13e1a5
molestador <hustlerxxl@gmail.com>
2017-02-05 19:46:38 +0200

    fsm/new feature: implement 1/2 of class

--- src/fsm.js
+++ src/fsm.js
@@ -1,32 +1,57 @@
 class FSM {
+
     /**
      * Creates new FSM instance.
      * @param config
      */
-    constructor(config) {}
+    constructor(config) {
+        if (!config) {
+            throw new Error()
+        }
+        this.config = config;
+        this.currentState = config.initial;
+
+
+    }
 
     /**
      * Returns active state.
      * @returns {String}
      */
-    getState() {}
+    getState() {
+        return this.currentState;
+    }
 
     /**
      * Goes to specified state.
      * @param state
      */
-    changeState(state) {}
+    changeState(state) {
+        if (!this.config.states[state]) {
+            throw new Error();
+        }
+        this.currentState = state;
+
+    }
 
     /**
      * Changes state according to event transition rules.
      * @param event
      */
-    trigger(event) {}
+    trigger(event) {
+        if (!this.config.states[this.currentState].transitions[event]) {
+            throw new Error();
+        }
+        this.changeState(this.config.states[this.currentState].transitions[event]);
 
+    }
     /**
      * Resets FSM state to initial.
      */
-    reset() {}
+    reset() {
+        this.changeState(this.config.initial)
+
+    }
 
     /**
      * Returns an array of states for which there are specified event transition rules.
@@ -34,26 +59,43 @@ class FSM {
      * @param event
      * @returns {Array}
      */
-    getStates(event) {}
+    getStates(event) {
+        if (!event) {
+            return Object.keys(this.config.states)
+        }
+        const eventStates = [];
+        const newStates = Object.keys(this.config.states)
+        for (var i = 0; i < newStates.length; i += 1) {
+            var keys = newStates[i];
+            if (this.config.states[keys].transitions[event]) {
+                eventStates.push(keys)
+            }
+        }
+        return eventStates;
+    }
+
+
+
+
 
     /**
      * Goes back to previous state.
      * Returns false if undo is not available.
      * @returns {Boolean}
      */
-    undo() {}
+    undo() { }
 
     /**
      * Goes redo to state.
      * Returns false if redo is not available.
      * @returns {Boolean}
      */
-    redo() {}
+    redo() { }
 
     /**
      * Clears transition history
      */
-    clearHistory() {}
+    clearHistory() { }
 }
 
 module.exports = FSM;
